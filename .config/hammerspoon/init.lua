a0b6df4fa190ae01aead55907b6463bb36083302
santiagohervella <57931354+santiagohervella@users.noreply.github.com>
2025-02-02 13:38:59 -0500

    Adding one line to hammerspoon to prevent display from ever going to sleep

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,1 +1,8 @@
+-- To stop complaints about missing globals hs
+---@diagnostic disable: undefined-global
+
 require("rcmd")
+
+-- Prevent the display from ever automatically going to sleep
+-- https://www.hammerspoon.org/docs/hs.caffeinate.html#set
+hs.caffeinate.set("displayIdle", true, true)
