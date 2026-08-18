a4a08f4bda150e0834c8064a23f084c1ab7b4cc1
santiagohervella <57931354+santiagohervella@users.noreply.github.com>
2026-08-17 20:00:14 -0400

    Tiny gitignore change

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -2,7 +2,7 @@
 ---@diagnostic disable: undefined-global
 
 -- require("rcmd")  -- Original implementation
-require("rcmd-v2")  -- New high-performance implementation
+require("rcmd-v2") -- New high-performance implementation
 
 -- Prevent the display from ever automatically going to sleep
 -- https://www.hammerspoon.org/docs/hs.caffeinate.html#set
